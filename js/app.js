ac3e6b5afb8fbfb433296e26efd99684c6d17c9c
Micawurfel <micawurfel@gmail.com>
2022-12-17 17:20:10 -0500

    get results

--- js/app.js
+++ js/app.js
@@ -1,36 +1,110 @@
-window.addEventListener('load', function() {
-    console.log('La página ha terminado de cargarse!!');
-});
+const piedra = document.getElementById("piedra")
+const papel = document.getElementById("papel")
+const tijera = document.getElementById("tijera")
 
-let piedra = document.getElementById("piedra")
-let papel = document.getElementById("papel")
-let tijera = document.getElementById("tijera")
+const gamerChoice = document.getElementById("gamer-option")
+const computerChoice = document.getElementById("computer-option")
+let gamer
+let computer
 
-let gamerOption = document.getElementById("gamer-option")
-let computerOption = document.getElementById("computer-option")
+const text = document.getElementById("text")
+let texto 
 
-let texto = document.getElementById("text")
+const gamerScore = document.getElementById("gamerScore")
+const computerScore = document.getElementById("computerScore")
+let gamerCount = 0
+let computerCount = 0
 
+// gamer choice
+const choices = ["piedra", "papel", "tijera"]
 
 piedra.onclick = () => {
-    gamerOption.classList.add('fa-hand-back-fist')
-    gamerOption.classList.remove('fa-hand')
-    gamerOption.classList.remove('fa-hand-scissors')
+    gamer = choices[0];
+    generateComputerChoice();
+    gamerChoice.innerHTML = `<i class="fa-solid fa-hand-back-fist"><i>`
+    getResult()
 }
 
 papel.onclick = () => {
-    gamerOption.classList.add('fa-hand')
-    gamerOption.classList.remove('fa-hand-back-fist')
-    gamerOption.classList.remove('fa-hand-scissors')
+    gamer = choices[1];
+    generateComputerChoice();
+    gamerChoice.innerHTML = `<i class="fa-solid fa-hand"><i>`
+    getResult()
 }
 
 tijera.onclick = () => {
-    gamerOption.classList.add('fa-hand-scissors')
-    gamerOption.classList.remove('fa-hand')
-    gamerOption.classList.remove('fa-hand-back-fist')
+    gamer = choices[2];
+    generateComputerChoice();
+    gamerChoice.innerHTML = `<i class="fa-solid fa-hand-scissors"><i>`
+    getResult()
 }
 
-const options = ["piedra", "papel", "tijera"]
+// computer choice
+function generateComputerChoice() {
+    let random = Math.floor(Math.random()* 3)
 
-let computerOpiton = Math.floor(Math.random()* 3)
-console.log(computerOpiton)
+    if (random === 0) {
+        computer = "piedra"
+        computerChoice.innerHTML = `<i class="fa-solid fa-hand-back-fist"><i>`
+    } else if (random === 1) {
+        computer = "papel"
+        computerChoice.innerHTML = `<i class="fa-solid fa-hand"><i>`
+    } else if (random === 2) {
+        computer = "tijera"
+        computerChoice.innerHTML = `<i class="fa-solid fa-hand-scissors"><i>`
+    }
+} 
+
+
+// get results
+function getResult(){
+
+    switch (gamer + computer) {
+        case "piedrapiedra":
+        case "papelpapel":
+        case "tijeratijera":
+            texto = "its a draw"
+            break;
+        case "piedratijera":
+        case "papelpiedra":
+        case "tijerapapel":
+            texto = `${gamer} beats ${computer}, you win!!`
+            gamerCount ++;
+            gamerScore.textContent = gamerCount
+            break;
+        case "piedrapapel":
+        case "papeltijera":
+        case "tijerapiedra":
+            texto = `${computer} beats ${gamer}, you lost!!`
+            computerScore.textContent = computerCount
+            computerCount ++;
+            break;
+    }
+
+    text.textContent = texto
+}
+
+// reset game 
+let startNewGame = document.querySelector("#reset")
+
+startNewGame.onclick = ()=>{
+    computerCount = 0;
+    gamerCount = 0; 
+    console.log ("new game")
+}
+
+function restartGame(){
+    if (computerCount === 4){
+        alert ("you lost the game, start new game")
+        computerCount = 0;
+        gamerCount = 0;
+    // }else if (gamerCount === 3) {
+    //     alert ("you won the game, start new game")
+    //     computerCount = 0;
+    //     gamerCount = 0;
+    } 
+};
+
+
+
+
